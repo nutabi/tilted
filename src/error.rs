9cae19a840a957c9f6681526680f1578a1cd2bf9
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-08-29 17:44:59 +0800

    Add a sub-error for unknown errors

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,19 @@
 //! This module implements the error types for `cal`.
 
-#[derive(Debug, Clone)]
-#[derive(thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum CalError {
     #[error("Lex error {0}")]
-    LexError(#[from] LexError)
+    LexError(#[from] LexError),
+
+    #[error("Unknown error {0}")]
+    Unknown(#[from] Box<dyn std::error::Error>),
 }
 
-#[derive(Debug, Clone)]
-#[derive(thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum LexError {
     #[error("Unrecognised character '{0}'")]
     UnrecognisedCharacter(char),
+
+    #[error("Unknown error: '{0}'")]
+    UnknownError(&'static str),
 }
