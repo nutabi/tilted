4bbb9ac8be7166d6e98b3cab74185025cf7d6e76
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-08-29 21:40:41 +0800

    Implement a complete lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,14 @@
 //! This modules implements a lexer, or tokeniser, for `cal`.
-//! 
+//!
 //! A lexer's job is to generate a stream of [`Token`]s from user input, which
 //! is used by the [`Parser`] to generate an Abstract Syntax Tree.
 
 use std::{ops::Index, slice::SliceIndex};
+
+use crate::{eof, error::LexError, token};
+
+/// Special [`Result`] type for the lexer.
+type Result<T> = std::result::Result<T, LexError>;
 
 /// Lexer for `cal`. It parses user input and return [`Token`]s.
 #[derive(Debug, Clone)]
@@ -16,25 +21,44 @@ pub struct Lexer {
 }
 
 /// Part of the source code tokenised. Returned by a [`Lexer`].
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Token {
+    /// Type of this [`Token`].
     kind: TokenKind,
+
+    /// Location of this [`Token`].
     span: Span,
 }
 
 /// Type of a [`Token`], also containing the information associated.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum TokenKind {
+    /// End-of-file token. Note that the [`Span`] associated with EOF is
+    /// out-of-bound, meaning if the span is used to look up source code, the
+    /// slice will be zero-length.
+    Eof,
+
+    /// Integer, i.e. numbers without decimal places.
     Int(u128),
+
+    /// Floating-point number, i.e. real numbers that are not integers.
     Float(f64),
+
+    /// Operator.
     Op(char),
-    Var(char),
+
+    /// Left parenthesis.
     LeftParen,
+
+    /// Right parenthesis.
     RightParen,
 }
 
+// Improve syntax clarity.
+use TokenKind::*;
+
 /// Spatial information of a [`Token`].
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Span {
     /// Index of the first character of this [`Span`].
     start_index: usize,
@@ -59,13 +83,177 @@ impl Index<Span> for Lexer {
     }
 }
 
+impl Iterator for Lexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.lex() {
+            Ok(token) if token.kind != Eof => Some(token),
+            _ => None,
+        }
+    }
+}
+
 impl Lexer {
     /// Creates a new [`Lexer`] from source code.
-    pub fn from_source_code<T: AsRef<str>>(source_code: T) -> Lexer
-    {
+    pub fn from_source_code<T: AsRef<str>>(source_code: T) -> Lexer {
         Lexer {
             source_code: Box::from(source_code.as_ref()),
             current_index: 0,
         }
+    }
+
+    /// Gets the next [`Token`] from source.
+    pub fn lex(&mut self) -> Result<Token> {
+        // Skip whitespaces.
+        for c in self.source_code.chars().skip(self.current_index) {
+            if c.is_whitespace() {
+                self.current_index += 1;
+            } else {
+                break;
+            }
+        }
+
+        // Check for EOF.
+        if self.current_index >= self.source_code.len() {
+            return Ok(eof!(self.current_index));
+        }
+
+        // Assign to handlers based on the next character.
+        match self
+            .source_code
+            .chars()
+            .nth(self.current_index)
+            .ok_or(LexError::InternalError(
+                "Unable to unwrap next character in source",
+            ))? {
+            // Numbers (integers and reals)
+            // Can start with a dot or number
+            '.' | '0'..='9' => self.handle_number(),
+
+            // Operators.
+            '+' | '-' | '*' | '/' => self.handle_operator(),
+
+            // Parentheses.
+            // These are short so they are handled in-place.
+            '(' => {
+                self.current_index += 1;
+                Ok(token!(LeftParen, self.current_index - 1, 1))
+            }
+            ')' => {
+                self.current_index += 1;
+                Ok(token!(RightParen, self.current_index - 1, 1))
+            }
+
+            // Any other characters
+            c => Err(LexError::UnrecognisedCharacter(c, self.current_index)),
+        }
+    }
+
+    pub fn handle_number(&mut self) -> Result<Token> {
+        // Keep track of the original index for later.
+        let original_index = self.current_index;
+
+        // Tracker for decimal place.
+        let mut seen_dot = false;
+
+        // Reserve enough space for a 100-char string.
+        // Most numbers (hopefully) are within this limit. However, we still
+        // need to cover the potential cases of more than 100 digits.
+        let mut result = String::with_capacity(100);
+        for c in self.source_code[self.current_index..].chars() {
+            match c {
+                '.' => {
+                    if !seen_dot {
+                        // Dot (if not seen)
+                        self.current_index += 1;
+                        seen_dot = true;
+                    } else {
+                        // Dot (if already seen)
+                        return Err(LexError::UnrecognisedCharacter(c, self.current_index));
+                    }
+                }
+
+                // Digit
+                c if c.is_ascii_digit() => {
+                    result.push(c);
+                    self.current_index += 1;
+                }
+
+                // Anything else
+                _ => break,
+            }
+        }
+
+        // Convert string to integer or float based on seen_dot.
+        if seen_dot {
+            // Float
+            let num = result
+                .parse::<f64>()
+                .map_err(|_| LexError::InternalError("Parse float failed"))?;
+
+            Ok(token!(Float(num), original_index, result.len()))
+        } else {
+            // Integer
+            let num = result
+                .parse::<u128>()
+                .map_err(|_| LexError::InternalError("Parse integer failed"))?;
+
+            Ok(token!(Int(num), original_index, result.len()))
+        }
+    }
+
+    pub fn handle_operator(&mut self) -> Result<Token> {
+        // Operator has only one char so it should be trivial.
+        let op = self
+            .source_code
+            .chars()
+            .nth(self.current_index)
+            .ok_or(LexError::InternalError("Unable to unwrap operator"))?;
+
+        // The parent match operator should have narrowed down the valid ones,
+        // but I think it is still important to check here, just in case I mess
+        // up somewhere else. Resources are cheap anyway :)
+        match op {
+            '+' | '-' | '*' | '/' => {
+                self.current_index += 1;
+                Ok(token!(Op(op), self.current_index - 1, 1))
+            }
+            _ => Err(LexError::InternalError(
+                "Invalid operator inside operator handler",
+            )),
+        }
+    }
+
+    /// Reverts this [`Lexer`] to its original state.
+    #[allow(unused)]
+    pub fn reset(&mut self) {
+        // Simply set the index to 0 to reset.
+        self.current_index = 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lexer() {
+        let source = "9 + 8 * 4 - (5 - 3)";
+        let mut lexer = Lexer::from_source_code(source);
+
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Int(9));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Op('+'));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Int(8));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Op('*'));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Int(4));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Op('-'));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::LeftParen);
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Int(5));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Op('-'));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::Int(3));
+        assert_eq!(lexer.next().unwrap().kind, TokenKind::RightParen);
+
+        assert!(lexer.next().is_none());
     }
 }
