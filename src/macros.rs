5b657cf04a5bedbeb9877da7171d050e4e9fac08
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-08-29 17:45:54 +0800

    Add macro `token`

--- src/macros.rs
+++ src/macros.rs
@@ -7,7 +7,21 @@ macro_rules! eof {
             span: crate::lexer::Span {
                 start_index: $idx,
                 end_index: $idx,
-            }
+            },
+        }
+    };
+}
+
+/// Quickly crafts a token of a specified kind of a known length and location.
+#[macro_export]
+macro_rules! token {
+    ($kind: expr, $start: expr, $length: expr) => {
+        crate::lexer::Token {
+            kind: $kind,
+            span: crate::lexer::Span {
+                start_index: $start,
+                end_index: $start + $length - 1,
+            },
         }
     };
 }
