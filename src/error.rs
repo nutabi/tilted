d05d550c86fdb439f61f5b5084321a58fbc50d1b
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-08-30 20:04:54 +0800

    Add `ParseError`

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,14 @@
 //! This module implements the error types for `cal`.
 
+use crate::TokenKind;
+
 #[derive(Debug, thiserror::Error)]
 pub enum CalError {
     #[error("Lex error {0}")]
     LexError(#[from] LexError),
+
+    #[error("Parse error {0}")]
+    ParseError(#[from] ParseError),
 
     #[error("Unknown error {0}")]
     Unknown(#[from] Box<dyn std::error::Error>),
@@ -16,4 +21,10 @@ pub enum LexError {
 
     #[error("Unknown error: '{0}'")]
     InternalError(&'static str),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParseError {
+    #[error("Expected number, found '{0:?}'")]
+    ExpectedNumber(TokenKind),
 }
