6a8fa94ec44e2417b6f5851c29aacc5300ed9943
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-08-30 13:27:28 +0800

    Reformat

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 #![deny(clippy::all)]
 #![warn(rustdoc::all)]
 
+pub mod ast;
 pub mod error;
 pub mod lexer;
 pub mod macros;
-pub mod ast;
 
+pub use ast::{BinaryNode, UnaryNode};
 pub use error::CalError;
 pub use lexer::{Lexer, Span, Token, TokenKind};
