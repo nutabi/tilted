409df2cdfb007e3a0a3568b0b0d61fb964dbaa30
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-09-04 23:36:18 +0800

    Implement a simple CLI

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,15 @@
 #![warn(rustdoc::all)]
 
 pub mod ast;
+#[cfg(feature = "cli")]
+pub mod cli;
 pub mod error;
 pub mod lexer;
 pub mod macros;
 pub mod parser;
 
 pub use ast::{BinaryAction, BinaryNode, NodeBox, Number, PlainNode, UnaryAction, UnaryNode};
+pub use cli::Cli;
 pub use error::{LexError, ParseError, TilError};
 pub use lexer::{Lexer, Operator, Span, Token, TokenKind};
 pub use parser::Parser;
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::process::exit;
+
+use tilted::{self, Cli};
+
+fn main() {
+    let cli: Cli = argh::from_env();
+    exit(cli.start().into())
+}
